f70266c55244b3b48a315a1086f7753e6624a27d
Yusef Napora <yusef@napora.org>
2021-08-12 22:11:06 -0400

    validate token on paste

--- main.js
+++ main.js
@@ -131,15 +131,17 @@ async function getImageMetadata(cid) {
   return { ...metadata, cid, gatewayURL, uri }
 }
 
-async function isTokenValid(token) {
+async function validateToken(token) {
   const web3storage = new Web3Storage({ token })
-  // cid for an empty IPFS directory
-  const testCID = 'bafybeiczsscdsbs7ffqz55asqdf3smv6klcw3gofszvwlyarci47bgf354'
-  const res = await web3storage.status(testCID)
-  if (res.status === 401 || res.status === 403) {
+
+  try {
+    for await (const _ of web3storage.list({ maxResults: 1 })) {
+      return true
+    }
+  } catch (e) {
+    console.log('invalid token', e)
     return false
   }
-  return true
 }
 
 // #endregion web3storage-interactions
@@ -466,19 +468,42 @@ function iconLabel(iconClass, labelText) {
 /**
  * DOM initialization for token management UI.
  */
-function setupTokenUI() {
+function setupTokenUI() {  
   if (!document.getElementById('token-ui')) {
     return
   }
 
-  tokenInput.onchange = evt => {
-    const token = evt.target.value
+  const tokenInputArea = document.getElementById('token-input-wrapper')
+  const tokenSpinner = document.getElementById('token-spinner')
+
+  const changeHandler = evt => {
+    let token
+    if (evt.type === 'paste' && evt.clipboardData) {
+      token = evt.clipboardData.getData('Text')
+    } else {
+      token = evt.target.value
+    }
     if (!token) {
       return
     }
-    saveToken(token)
-    updateTokenUI()
+
+    hideElement(tokenInputArea)
+    showElement(tokenSpinner)
+
+    validateToken(token).then(valid => {
+      hideElement(tokenSpinner)
+      if (!valid) {
+        showPopupMessage('Invalid token!')
+        updateTokenUI()
+        return
+      }
+      saveToken(token)
+      updateTokenUI()
+    })
   }
+
+  tokenInput.onchange = changeHandler
+  tokenInput.onpaste = changeHandler
 
   const tokenDeleteButton = document.getElementById('token-delete-button')
   if (tokenDeleteButton) {
